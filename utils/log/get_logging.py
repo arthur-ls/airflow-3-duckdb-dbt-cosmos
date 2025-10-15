323876c907b41dfc951fd324de003db046d5b9e7
Arthur Lima <arthurlimadossantos@gmail.com>
2025-10-15 20:53:48 -0300

    feat: utils

--- utils/log/get_logging.py
+++ utils/log/get_logging.py
@@ -5,7 +5,7 @@ class Logger:
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
         handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s: %(name)s: %(levelname)s: %(message)s')
         handler.setFormatter(formatter)
         self.logger.addHandler(handler)
 
