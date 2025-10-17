127643421e0bf89411c639228bd918aac161c37b
Arthur Lima <arthurlimadossantos@gmail.com>
2025-10-17 20:40:02 -0300

    chore: change dag

--- dags/soccer_dag/dag.py
+++ dags/soccer_dag/dag.py
@@ -2,34 +2,33 @@ from airflow.sdk import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
 from datetime import datetime
 from cosmos import DbtTaskGroup, ProfileConfig, ProjectConfig
+from cosmos.config import RenderConfig
 from airflow.providers.standard.operators.python import PythonOperator
 from utils.yaml.yaml_reader import get_yaml_paths
 from task.operator import operator_executor
+from utils.yaml.yaml_reader import read_yaml_file
 from pathlib import Path
 import os
 
 YAML_FOLDER = os.path.dirname(__file__) + "/*.yml"
-DBT_PATH = "/opt/airflow/dbt/dbt_duckdb_iceberg"
-DBT_PROFILE = "dbt_duckdb_iceberg"
-DBT_TARGETS = "dev"
 MAX_ACTIVE_RUNS = 1
 TAGS = ['DBT', 'Cosmos', 'Campeonato Brasileiro', 'Kaggle']
 DAG_ID = 'dbt-kaggle-campeonato_brasileiro'
 
-def get_dbt_configs():
+def get_dbt_configs(dbt_target: str, dbt_profile: str, dbt_path: str, db_path: str):
     profile_config = ProfileConfig(
-        profile_name=DBT_PROFILE,
-        target_name=DBT_TARGETS,
-        profiles_yml_filepath=Path(f"{DBT_PATH}/profiles.yml"),
+        profile_name=dbt_profile,
+        target_name=dbt_target,
+        profiles_yml_filepath=Path(f"{dbt_path}/profiles.yml"),
     )
 
     project_config = ProjectConfig(
-        dbt_project_path=DBT_PATH,
-        models_relative_path="models"
+        dbt_project_path=dbt_path,
+        env_vars={"DUCKDB_PATH": db_path}
     )
     return profile_config, project_config
 
-def create_dag(file: str, project_config, profile_config):
+def create_dag(file: str, project_config, profile_config, db_path: str):
     default_args = {
         "owner": "arthur.santos",
         "start_date": datetime(2022, 1, 1),
@@ -41,6 +40,8 @@ def create_dag(file: str, project_config, profile_config):
         default_args=default_args,
         schedule=None,
         catchup=False,
+        max_active_runs=1,
+        tags=TAGS
     )
 
     with dag:
@@ -57,16 +58,16 @@ def create_dag(file: str, project_config, profile_config):
            group_id="dbt_running_models",
            project_config=project_config,
            profile_config=profile_config,
-           default_args={"retries": 2},
+           default_args={"retries": 2, "pool": "one_each_time"},
            dag=dag,
         )
 
         start_dag >> prepare_source_data >> dbt_running_models >> end_dag
 
-        # start_dag >> prepare_source_data >> end_dag
-
     return dag
 
-profile_config, project_config = get_dbt_configs()
 for file in get_yaml_paths(YAML_FOLDER):
-    create_dag(file, project_config=project_config, profile_config=profile_config)
+    yaml_data = read_yaml_file(file)
+    db_path = yaml_data['db_path']
+    profile_config, project_config = get_dbt_configs(yaml_data['dbt_target'], yaml_data['dbt_profile'], yaml_data['dbt_path'], db_path=db_path)
+    create_dag(file, project_config=project_config, profile_config=profile_config, db_path=db_path)
