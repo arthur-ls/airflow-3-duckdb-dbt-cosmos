efa475cead1f0a90cd3852c681bd0ab531da062c
Arthur Lima <arthurlimadossantos@gmail.com>
2025-10-17 20:41:38 -0300

    chore: change operator

--- task/operator.py
+++ task/operator.py
@@ -4,10 +4,10 @@ from utils.yaml.yaml_reader import read_yaml_file
 
 def operator_executor(yaml_path: str):
     yaml_data = read_yaml_file(yaml_path)
+    print(yaml_data)
 
     import_data = ImportData(kaggle_project=yaml_data['kaggle_project'])
     db_connect = DuckDBSaveDatabase(db_path=yaml_data['database'])
-    db_connect.connect_persistent_db()
     db_connect.create_schema(yaml_data['schema'])
 
     for file in yaml_data['file_path']:
